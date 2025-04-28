5fca0524cc68ba6d995b6d6953da696da0d72e9c
Santosh Bhaykatte <bhaykatte2020@gmail.com>
2025-04-29 02:28:25 +0530

    Initial Project Setup & Testing Code Push

---
+++ src/main/java/com/rentmate/controller/HomeServlet.java
@@ -0,0 +1,17 @@
+package com.rentmate.controller;
+
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/home")
+public class HomeServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.getWriter().println("<h1>Welcome to RentMate!</h1>");
+    }
+}
